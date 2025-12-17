26969db3fb3f235b74e95ef656907479a34467c0
lillyheather <lillylixu2@gmail.com>
2025-12-17 14:38:07 +0000

    quotes

--- Doze.App/src/components/Homepage.vue
+++ Doze.App/src/components/Homepage.vue
@@ -9,11 +9,26 @@ defineProps({
 })
 
 const quotes = [
-    "\"The only way to do great work is to love what you do.\" - Steve Jobs",
-    "\"Success is not the key to happiness. Happiness is the key to success.\" - Albert Schweitzer",
-    "\"Believe you can and you're halfway there.\" - Theodore Roosevelt",
-    "\"Your time is limited, so don't waste it living someone else's life.\" - Steve Jobs",
-    "\"The best way to predict the future is to invent it.\" - Alan Kay"
+"\"Sometimes the worst place you can be is in your own head.\" — Timothy Ferriss",
+"\"You are stronger than you think. You have gotten through every bad day in your life, and you are undefeated.\" — Lori Gottlieb",
+"\"Fall seven times, stand up eight.\" — Japanese Proverb",
+"\"The greatest weapon against stress is our ability to choose one thought over another.\" — William James",
+"\"You may encounter many defeats, but you must not be defeated.\" — Maya Angelou",
+"\"The sun himself is weak when he first rises, and gathers strength and courage as the day gets on.\" — Charles Dicken",
+"\"Success is not final, failure is not fatal: It is the courage to continue that counts.\" — Winston Churchill",
+"\"You have been criticizing yourself for years, and it hasn’t worked. Try approving of yourself and see what happens.\" — Louise Hay",
+"\"You are your best thing.\" — Toni Morrison",
+"\"Healing takes time, and asking for help is a courageous step.\" — Mariska Hargitay",
+"\"Even the darkest night will end, and the sun will rise.\" — Victor Hugo",
+"\"Happiness can be found, even in the darkest of times, if one only remembers to turn on the light.\" — J.K. Rowling",
+"\"There is hope, even when your brain tells you there isn’t.\" — John Green",
+"\"If you’re going through hell, keep going.\" — Winston Churchill",
+"\"Hardships often prepare ordinary people for an extraordinary destiny.\" — C.S. Lewis",
+"\"What mental health needs is more sunlight, more candor, and more unashamed conversation.\" — Glenn Close",
+"\"Turn your wounds into wisdom.\" — Oprah Winfrey",
+"\"The only way to get rid of the fear of doing something is to go out and do it.\" — Susan Jeffers",
+"\"Vulnerability is not winning or losing; it’s having the courage to show up and be seen when we have no control over the outcome.\" — Brené Brown",
+"\"You can’t stop the waves, but you can learn to surf.\" — Jon Kabat-Zinn"
 ];
 
 const quote = ref(quotes[Math.floor(Math.random() * quotes.length)]);
