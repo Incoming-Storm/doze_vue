e7bd68d96a134199689b85f22e1d518e1dec9885
Incoming-Storm <93nightstar7@gmail.com>
2026-02-26 15:23:42 +0000

    t

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
+
   publicPath: process.env.NODE_ENV === "production" ? "/doze_vue/" : "/",
-=======
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
->>>>>>> fe9e93ab0b779123a641ae67c3d6fa8225976376
-};
+}
